2b550987e7ab0cdc855087699e94ef5350614fd3
GodexPro <adityaagarwal110408@gmail.com>
2021-01-30 16:38:52 +0530

    done

--- Bird.js
+++ Bird.js
@@ -1,12 +1,23 @@
-class Bird extends BaseClass {
-  constructor(x,y){
-    super(x,y,50,50);
-    this.image = loadImage("sprites/bird.png");
-  }
-
-  display() {
-    //this.body.position.x = mouseX;
-    //this.body.position.y = mouseY;
-    super.display();
-  }
+class Bird{
+  constructor(x, y) {
+      var options = {
+          'restitution':0.8,
+          'friction':2,
+          'density':2
+      }
+      this.body = Bodies.rectangle(x, y, 50,50, options);
+      this.width = 50;
+      this.height = 50;
+      this.image = loadImage("sprites/bird.png");
+      World.add(world, this.body);
+    }
+    display(){
+      var angle = this.body.angle;
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+    }
 }
